6bdfa3a750050f634b8b1aa19c5e17e519e72f45
Fabrizio Lazzaretti <fabrizio.lazzaretti@immosparrow.ch>
2022-07-23 12:00:04 +0200

    add panel

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { MQExplorePanel } from './panels/MQExplorePanel';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
+	const explorerPanel = new MQExplorePanel(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(MQExplorePanel.viewType, explorerPanel));
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "message-queue-explorer" is now active!');
---
+++ src/panels/MQExplorePanel.ts
@@ -0,0 +1,53 @@
+import * as vscode from "vscode";
+
+export class MQExplorePanel implements vscode.WebviewViewProvider {
+  public static readonly viewType = "message-queue-explorer.explorView";
+
+  private _view?: vscode.WebviewView;
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    // webviewView.webview.onDidReceiveMessage((data) => {
+    //   switch (data.type) {
+    //     case "colorSelected": {
+    //       vscode.window.activeTextEditor?.insertSnippet(
+    //         new vscode.SnippetString(`#${data.value}`)
+    //       );
+    //       break;
+    //     }
+    //   }
+    // });
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    return /*html*/ `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Hello World!</title>
+        </head>
+        <body>
+          <h1>Hello World!</h1>
+        </body>
+      </html>
+    `;
+  }
+}
