d23f0d31bf382a7ae0e6a6ab63e6fa19f383c11a
Fabrizio Lazzaretti <fabrizio.lazzaretti@immosparrow.ch>
2022-07-23 13:59:48 +0200

    connect to service bus

--- src/createConnection/CreateConnectionQuickPick.ts
+++ src/createConnection/CreateConnectionQuickPick.ts
@@ -1,12 +1,31 @@
-import { window, commands, ExtensionContext } from 'vscode';
+import { ServiceBusClient } from "@azure/service-bus";
+import { window, commands, ExtensionContext } from "vscode";
+import * as vscode from 'vscode';
 
-export async function createConnectionQuickPick(){
-
-    const selection = await window.showQuickPick([
-        {
-            label: 'Service Bus',
-            execute: () => window.showInputBox({placeHolder: "connectionString"}),
+export async function createConnectionQuickPick() {
+  const selection = await window.showQuickPick([
+    {
+      label: "Service Bus with Connection String",
+      execute: async () => {
+        const connectionString = await window.showInputBox({
+          title: "Add ServiceBus Connection by Connection String",
+          placeHolder: "ServiceBus Connection String",
+        });
+        if (connectionString) {
+            vscode.window.withProgress({title: 'try to connect..',location: vscode.ProgressLocation.Notification,}, async () => {
+            try{
+                const serviceBusClient = new ServiceBusClient(connectionString);
+                const name = serviceBusClient.fullyQualifiedNamespace;
+                await serviceBusClient.close();
+                vscode.window.showInformationMessage(`connected successful to ${name}`);
+            }
+            catch{
+                vscode.window.showErrorMessage("failed to connect");
+            }
+        });
         }
-    ]);
-    selection?.execute();
+      },
+    },
+  ]);
+  selection?.execute();
 }
