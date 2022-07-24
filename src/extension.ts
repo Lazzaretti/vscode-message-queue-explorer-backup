3bb9ba5cc6a17c6a4e3ffff40b6c84173341c423
Fabrizio Lazzaretti <fabrizio.lazzaretti@immosparrow.ch>
2022-07-24 11:50:54 +0200

    remove hello world

--- src/extension.ts
+++ src/extension.ts
@@ -28,21 +28,6 @@ export function activate(context: vscode.ExtensionContext) {
 		const panel = new MessagesWebView(connectionFacade, context.extensionUri);
 		panel.open(args);
 	}));
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "message-queue-explorer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('message-queue-explorer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from message-queue-explorer!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
